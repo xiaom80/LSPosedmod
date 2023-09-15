5f4ba315d83c007a9c6491d662cc460b448ece74
yanghua <yanghua@1235448156644463.onaliyun.com>
2023-09-15 11:59:56 +0800

    'fix:magisk-loader'

--- magisk-loader/src/main/java/org/lsposed/lspd/core/Main.java
+++ magisk-loader/src/main/java/org/lsposed/lspd/core/Main.java
@@ -30,13 +30,13 @@ import org.lsposed.lspd.BuildConfig;
 public class Main {
 
     public static void forkCommon(boolean isSystem, String niceName, String appDir, IBinder binder) {
-        Startup.initXposed(isSystem, niceName, appDir, ILSPApplicationService.Stub.asInterface(binder));
+        Startup.initgeekposed(isSystem, niceName, appDir, ILSPApplicationService.Stub.asInterface(binder));
         if ((niceName.equals(BuildConfig.MANAGER_INJECTED_PKG_NAME) || niceName.equals(BuildConfig.DEFAULT_MANAGER_PACKAGE_NAME))
                 && ParasiticManagerHooker.start()) {
             Utils.logI("Loaded manager, skipping next steps");
             return;
         }
         Utils.logI("Loading geekposed for " + niceName + "/" + Process.myUid());
-        Startup.bootstrapXposed();
+        Startup.bootstrapgeekposed();
     }
 }
